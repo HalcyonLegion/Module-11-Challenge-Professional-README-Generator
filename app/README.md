9b17951def597340b99a2815a7fb59ca1fe2b472
Lee Webb <leewerbb55@hotmail.com>
2023-05-08 21:12:35 +0800

    It's alive? New files overwrite old, badge working

--- app/README.md
+++ app/README.md
@@ -1,49 +1,33 @@
 # test
+
+  ![](https://img.shields.io/badge/license-MIT-green)
   
   ## Description
   test
 
   ## Table of Contents
   - [Installation](#installation)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  - [Usage](#usage)
+  - [License](#license)
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [Questions](#questions)
 
   ## Installation
   test
 
-  
-  
+  ## Usage
+  test
+
+  ## License
+  This project is licensed under the MIT license.
+
+  ## Contributing
+  test
+
+  ## Tests
+  test
+
+  ## Questions
+  If you have any questions about this project, please reach out to us at test or check out my GitHub profile for more [test](https://github.com/test).
   
--- app/utils/generateMarkdown.js
+++ app/utils/generateMarkdown.js
@@ -1,9 +1,11 @@
 // function to generate markdown for README
 // Created a full test to check if this works as intended, it seems so. The array I initially assumed would be questions.title but it's actually data.title.
 
-// Where/What is the license badge??
+// Where/What is the license badge?? Tried adding going to test now.
 function generateMarkdown(data) {
     return `# ${data.title}
+
+  ![](https://img.shields.io/badge/License-${encodeURIComponent(data.license)}-green)
   
   ## Description
   ${data.description}
@@ -25,8 +27,14 @@ function generateMarkdown(data) {
   ## License
   This project is licensed under the ${data.license} license.
 
-  
-  
+  ## Contributing
+  ${data.contributing}
+
+  ## Tests
+  ${data.tests}
+
+  ## Questions
+  If you have any questions about this project, please reach out to us at ${data.email} or check out my GitHub profile for more [${data.githubUsername}](https://github.com/${data.githubUsername}).
   `;
   }
   
