504edcab0e5465eb9a402a5cc191a6dd1c0875ac
Lee Webb <leewerbb55@hotmail.com>
2023-05-08 20:38:36 +0800

    Updated comments, worked out what to do for the table of contents and the questions section

--- app/index.js
+++ app/index.js
@@ -4,7 +4,7 @@ const path = require("path");
 const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
 
-// array of questions for user
+// array of questions for the user, other bits like the questions section and the table of contents can be handled in the other JS file.
 const questions = [
     {
         type: 'input',
@@ -26,6 +26,33 @@ const questions = [
         name: 'usage',
         message: 'Provide usage instructions:',
     },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Choose a license for your project:',
+        choices: ['MIT', 'Apache 2.0', 'GPL 3.0', 'BSD 3', 'None'],
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        message: 'Provide contribution guidelines:',
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'Provide testing instructions:',
+    },
+    {
+        type: 'input',
+        name: 'githubUsername',
+        message: 'Enter your GitHub username:',
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Enter your email address:',
+    },
+];
 
 // function to write README file
 function writeToFile(fileName, data) {
